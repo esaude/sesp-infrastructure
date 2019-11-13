343a5dfa2ade16e2edfc1e4bd20c6c04ea51430d
Joao machiana <joaomachiana@gmail.com>
2019-11-13 16:15:06 +0200

    Fixed automatic backup scripts, added new path

--- backup-admin/backup-bahmni-cronjob.sh
+++ backup-admin/backup-bahmni-cronjob.sh
@@ -1,52 +1,62 @@
 #!/bin/sh
 
+echo "Starting backup" 
 #Definir variaveis
 
+echo "Creating variables for backup"
 # Preparacao das variaveis.
 ###########################
 
 # Atribui o ano atual a variavel ano.
 
-ano=$(date +%y)
+year=$(date +%Y)
 
 # Atribui o mes atual a variavel mes.
 
-mes=$(date +%m)
+month=$(date +%m)
 
 # Atribui o dia atual a variavel dia.
 
-dia=$(date +%d)
+day=$(date +%d)
+
+# SESP backup folder
+sesp_backup='/sesp-backup'
 
 # Cria o diretorio de backup.
-sudo mkdir /opt/sesp
-sudo mkdir /opt/sesp/$dia$mes$ano
+mkdir -p $sesp_backup/$day$month$year
 
 #date %F\ %T
 
+echo "Backing up OpenMRS database"
+
+# OpenMRS backup
+
 # Para backup do mysql
-cd /data/openmrs/
+cd /data/
 #Executar o comando as todos dias as 00hrs
 
 # run the comand
-sudo  bahmni -i local backup --backup_type=db --options=openmrs
+bahmni -i local backup --backup_type=db --options=openmrs
 
-#Para backup no postgres
- #cd /var/lib/pgbackrest/new_backup
+# compress the backup folder
+tar -zcvf openmrs-$day$month$year.tar.gz openmrs/
 
-# run the comand
-# sudo bahmni -i local backup --backup_type=db --options=postgres
+# move the compressed openmrs backup folder to SESP-TR backup directory
+mv openmrs-$day$month$year.tar.gz $sesp_backup/$day$month$year
 
+echo "Backing up Postgres database"
 
+# Postgres OpenELIS/OpenERP backup
+cd /var/lib/
 
-#copia os arquivos para o diretorio de backup neste caso o sesp.
-# Em vez de copia pode ser feito também um TAR.
+# remove existing backup folder
+rm -rf pgbackrest/
 
-sudo cp -R /data/openmrs/2019* /opt/sesp/$dia$mes$ano
-sudo cp -R /var/lib/pgbackrest/* /opt/sesp/$dia$mes$ano
+# Create backup of postgres
+bahmni -i local backup --backup_type=db --options=postgres
 
-# Zipar todos os arquivos existentes nessa pasta
-cd /opt/sesp/$dia$mes$ano
-#find . -type d -exec zip {}.zip {} \; 
-#sudo tar -czvf backup.tar.gz -r ./
-sudo zip backup_$dia$mes$ano -r 2019* 
-ls -l
+# compress postgres backup
+tar -zcvf -r pgbackrest-$day$month$year.tar.gz pgbackrest/
+
+# move the compressed postgres backup folder to SESP-TR backup directory
+mv pgbackrest-$day$month$year.tar.gz $sesp_backup/$day$month$year
