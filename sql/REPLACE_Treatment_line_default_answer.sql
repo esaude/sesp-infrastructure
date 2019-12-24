06a3843c6a4dbda76ac15b3713d4cf00ce1466ce
Edgêncio da Calista <edgencio@gmail.com>
2019-12-24 16:40:00 +0200

    Replace Treatment_line default answer

---
+++ sql/REPLACE_Treatment_line_default_answer.sql
@@ -0,0 +1,1 @@
+UPDATE drug_order_relationship SET treatment_line_id = (select concept_id from concept where   uuid = 'c7767178-2652-11ea-978f-2e728ce88125') WHERE treatment_line_id = (select concept_id from concept where   uuid = 'e53fd4bf-b89c-4f97-9c82-120038ea435c');
