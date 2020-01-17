dc2470deb7713c6d383ce5868e88f3d047e27dad
Edgêncio da Calista <edgencio@gmail.com>
2020-01-17 10:59:37 +0200

    Remove patient duplications

---
+++ sql/1_MAIO_Remove_Patient_Duplications.sql
@@ -0,0 +1,7 @@
+/* NOTE THA THIS SCRIPT SHOULD ONLY BE EXECUTED AT 1o DE MAIO HEALTH FACILITY
+
+-- Remove duplicate identifier for patient id 209499*/
+delete from patient_identifier where identifier = '12/1450';
+
+-- Remove multiple person address that are causing patient duplication!
+delete from person_address where person_id in (2161,2162,2163,2164);
