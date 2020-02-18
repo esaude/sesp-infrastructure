e2692fe14cf83d66270cadd2a87293ca68fed984
amosh101 <amosh101@outlook.com>
2020-02-18 11:37:54 +0200

    Update queries

--- sql/change_patient_NID_status.sql
+++ sql/change_patient_NID_status.sql
@@ -1,29 +1,35 @@
 -- FIX NID
--- select patient_id from patient_identifier where identifier = '0110010901/01/2020/00116';
 
+-- find patient id
+select patient_id from patient_identifier where identifier = '0110010901/01/2020/00116';
+
+-- use patient id to update nid
 update patient_identifier set identifier = '0110011701/01/2016/00188' 
-where patient_id = (select patient_id from patient_identifier where identifier = '0110010901/01/2020/00116') and identifier_type = 3;
+where patient_id = <valor de patient id aqui> and identifier_type = 3;
+
 
 -- FIX STATUS_STATE
--- select * from patient_status_state;
+-- find status state
+select * from patient_status_state where patient_id = <valor de patient id aqui>;
 
+-- update status state
 update patient_status_state set patient_state = 'ACTIVE' 
-where patient_id = (select patient_id from patient_identifier where identifier = '0110011701/01/2016/00188');
+where patient_id = <valor de patient id aqui>;
 
 
 -- FIX STATUS ATTRIBUTE TYPE
 /*select person_attribute_type_id from person_attribute_type where name = 'TYPE_OF_REGISTRATION';
-select * from concept_name where concept_id = '7980'; 
-select * from concept_answer where concept_id = '7982';
+select concept_id from concept_name where name = 'TRANSFERRED_PATIENT'*/
 
+-- find attribute type concept
 select * from person_attribute 
 where person_attribute_type_id = (select person_attribute_type_id from person_attribute_type where name = 'TYPE_OF_REGISTRATION')
 and person_id = (select patient_id from patient_identifier where identifier = '0110011701/01/2016/00188');
-*/ 
 
-update person_attribute set value = '7981' 
+-- update status
+update person_attribute set value = (select concept_id from concept_name where name = 'TRANSFERRED_PATIENT')
 where person_attribute_type_id = (select person_attribute_type_id from person_attribute_type where name = 'TYPE_OF_REGISTRATION')
-and person_id = (select patient_id from patient_identifier where identifier = '0110011701/01/2016/00188');
+and person_id = <valor de patient id aqui>;
 
 
 
